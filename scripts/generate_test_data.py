c0f842fc23d07192427652b45621ed3f7ee01178
Riley Weber <rileyw13@protonmail.com>
2019-10-27 12:57:00 -0700

    added option to use exponential scale (not yet implemented)

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -29,14 +29,20 @@ def create_argparser() -> argparse.ArgumentParser:
         'matrices to test speed of LU decomposition')
     parser.add_argument('upper_limit', 
                         help='Maximum dimension of generated matrices')
+    parser.add_argument('-e', '--exponential', dest='exponential',
+                        action='store_const', const=True, default=False,
+                        help='use an exponential scale (default: linear)')
     parser.add_argument('-l', '--lower_limit', default=None, nargs='?',
                         help='Minimum dimension of generated matrices')
     parser.add_argument('-s', '--step', dest='step', default=None, nargs='?',
                         help='Step in dimension of generated matrices whose ' \
                             'sizes increase linearly')
-    parser.add_argument('-r', '--ratio', dest='ratio', default=None, nargs='?',
+    parser.add_argument('-r', '--ratio', dest='ratio', default=2, nargs='?',
                         help='Ratio of increase of dimension in generated '\
-                            'matrices whose sizes increase exponentially')
+                            'matrices whose sizes increase exponentially. '\
+                            'Matrix sizes will be r^n, where n increases '\
+                            'until r^n is greater than upper_limit. Only '\
+                            'works with -e')
     parser.add_argument('-o', '--output_file', dest='output_file',
                         default='test_matrices.hpp', nargs='?',
                         help='File to save output to')
@@ -46,13 +52,21 @@ def create_argparser() -> argparse.ArgumentParser:
 def prepare_args(args: Args) -> Args:
     ## convert to integers, but convert to float first to allow use of
     ## scientific notation like '1e6'
+
     args.upper_limit = int(float(args.upper_limit))
-    if args.step is None:
-        args.step = args.upper_limit/10
-    if args.lower_limit is None:
-        args.lower_limit = args.step
+
+    if args.exponential:
+        args.ratio = int(float(args.ratio))
+        args.lower_limit = args.ratio
+    else:
+        if args.step is None:
+            args.step = args.upper_limit/10
+        if args.lower_limit is None:
+            args.lower_limit = args.step
+        args.step = int(float(args.step))
+
     args.lower_limit = int(float(args.lower_limit))
-    args.step = int(float(args.step))
+
     return args
 
 
