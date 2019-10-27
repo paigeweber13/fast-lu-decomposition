854c5258017ed81997f95eb739d555430977fe64
Riley Weber <rileyw13@protonmail.com>
2019-10-27 12:23:29 -0700

    generates matrices and converts them to cpp literals

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -1,4 +1,5 @@
 import argparse
+import random
 
 HPP_BOILERPLATE = "#pragma once\n" \
     + "\n" \
@@ -33,15 +34,46 @@ def create_argparser() -> argparse.ArgumentParser:
     return parser
 
 
-def main():
-    parser = create_argparser()
-    args = parser.parse_args()
+def generate_matrix(n: int) -> [[]]:
+    result = []
+    for i in range(n):
+        result.append([])
+        for _ in range(n):
+            result[i].append(random.random())
+    return result
 
+
+def matrix_as_cpp_literal(matrix: [[]]) -> str:
+    result =  '{\n'
+    for i in range(len(matrix)):
+        result += '{'
+        for j in range(len(matrix)):
+            result += str(matrix[i][j]) + ','
+        result += '},\n'
+    result += '},\n'
+    return result
+
+
+def main():
+    args = create_argparser().parse_args()
+
+    # convert to integers, but convert to float first to allow use of
+    # scientific notation like '1e6'
+    args.upper_limit = int(float(args.upper_limit))
+    args.lower_limit = int(float(args.lower_limit))
     if(args.step == None):
-        args.step = float(args.upper_limit)/10
+        args.step = args.upper_limit/10
+    args.step = int(float(args.step))
 
     output_text = HPP_BOILERPLATE \
-                + VECTOR_OF_MATRICES_DECLARATION
+                + VECTOR_OF_MATRICES_START \
+
+    for n in range(args.lower_limit, args.upper_limit, args.step):
+        output_text += matrix_as_cpp_literal(generate_matrix(n))
+
+    output_text += VECTOR_OF_MATRICES_END
+
+    print(output_text)
 
 
 if(__name__ == '__main__'):
