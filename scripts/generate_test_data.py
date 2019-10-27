80074eaebfe49a4e3dc84ffd8cc3187da36b0712
Riley Weber <rileyw13@protonmail.com>
2019-10-27 12:42:01 -0700

    preparing to support  exponential scales. moved prepare_args to own function

--- scripts/generate_test_data.py
+++ scripts/generate_test_data.py
@@ -4,6 +4,7 @@ import random
 from typing import List
 
 Matrix = List[List[float]]
+Args = argparse.Namespace
 
 HPP_BOILERPLATE = "#pragma once\n" \
     + "\n" \
@@ -26,16 +27,32 @@ VECTOR_OF_MATRICES_END = "});\n"
 def create_argparser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(description='Generate square ' +\
         'matrices to test speed of LU decomposition')
-    parser.add_argument('lower_limit', 
-                        help='Minimum dimension of generated matrices')
     parser.add_argument('upper_limit', 
                         help='Maximum dimension of generated matrices')
+    parser.add_argument('-l', '--lower_limit', default=None, nargs='?',
+                        help='Minimum dimension of generated matrices')
     parser.add_argument('-s', '--step', dest='step', default=None, nargs='?',
-                        help='Step in dimension of generated matrices')
+                        help='Step in dimension of generated matrices whose ' \
+                            'sizes increase linearly')
+    parser.add_argument('-r', '--ratio', dest='ratio', default=None, nargs='?',
+                        help='Ratio of increase of dimension in generated '\
+                            'matrices whose sizes increase exponentially')
     parser.add_argument('-o', '--output_file', dest='output_file',
                         default='test_matrices.hpp', nargs='?',
                         help='File to save output to')
     return parser
+
+
+def prepare_args(args: Args) -> Args:
+    ## convert to integers, but convert to float first to allow use of
+    ## scientific notation like '1e6'
+    args.upper_limit = int(float(args.upper_limit))
+    if args.step is None:
+        args.step = args.upper_limit/10
+    if args.lower_limit is None:
+        args.lower_limit = args.step
+    args.lower_limit = int(float(args.lower_limit))
+    args.step = int(float(args.step))
 
 
 def generate_matrix(n: int) -> Matrix:
@@ -60,15 +77,7 @@ def matrix_as_cpp_literal(matrix: Matrix) -> str:
 
 def main():
     # Process args
-    args = create_argparser().parse_args()
-
-    ## convert to integers, but convert to float first to allow use of
-    ## scientific notation like '1e6'
-    args.upper_limit = int(float(args.upper_limit))
-    args.lower_limit = int(float(args.lower_limit))
-    if args.step == None:
-        args.step = args.upper_limit/10
-    args.step = int(float(args.step))
+    args = process_args(create_argparser().parse_args())
 
     # Build header file with text in it
     output_text = HPP_BOILERPLATE \
