a9bb14c5e9f787e0c732e7208996b0762745c5b5
Riley Weber <rileyw13@protonmail.com>
2019-10-27 13:27:37 -0700

    created stub for basic matrix generator

---
+++ src/generate_test_matrices.cpp
@@ -0,0 +1,7 @@
+#include "generate_test_matrices.hpp"
+
+vector<vector<
+  float, boost::alignment::aligned_allocator<float, 32> 
+>> generate_matrix(size_t k){
+
+}
---
+++ src/generate_test_matrices.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <boost/align/aligned_allocator.hpp>
+#include <vector>
+
+using namespace std;
+
+// note: a matrix of floats of size 32e9 will require about 128GB to represent
+// in memory
+
+vector<vector<
+  float, boost::alignment::aligned_allocator<float, 32> 
+>> generate_matrix(size_t k);
