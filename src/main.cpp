dbde2647951f1a4d525991a062edf94f55c9a056
Riley <rileyw13@protonmail.com>
2019-11-04 12:56:22 -0500

    added single hard-coded test, compare function

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 void test();
+bool compare_matrices(Matrix a, Matrix b);
 
 int main(){
   test();
@@ -22,6 +23,21 @@ void print_matrix(Matrix m){
   cout << endl;
 }
 
+bool compare_matrices(Matrix a, Matrix b){
+  if(a.size() != b.size() || a[0].size() != b[0].size())
+    return false;
+
+  for(size_t i = 0; i < a.size(); i++){
+    for(size_t j = 0; j < a[0].size(); j++){
+      if(a[i][j] != b[i][j]){
+        // cout << "a[i][j]: " << a[i][j] << " b[i][j]: " << b[i][j] << endl;
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
 void test(){
   auto m = generate_matrix(8);
   cout << "original matrix:" << endl;
@@ -34,4 +50,28 @@ void test(){
   print_matrix(l);
   cout << "upper diagonal:" << endl;
   print_matrix(u);
+
+
+  m = {{1, 1, -1},
+       {1, -2, 3},
+       {2, 3, 1}};
+  Matrix expected_l = {{1, 0, 0},
+                       {1, 1, 0},
+                       {2, -1./3., 1}};
+  Matrix expected_u = {{1, 1, -1},
+                       {0, -3, 4},
+                       {0, 0, 13./3.}};
+  cout << "original matrix:" << endl;
+  print_matrix(m);
+
+  lu = lu_factorize(m);
+  l = lu.first;
+  u = lu.second;
+  cout << "lower diagonal:" << endl;
+  print_matrix(l);
+  cout << "upper diagonal:" << endl;
+  print_matrix(u);
+  cout << "result == expected?" << endl;
+  cout << "lower: " << compare_matrices(l, expected_l) << " ";
+  cout << "upper: " << compare_matrices(u, expected_u) << endl;
 }
