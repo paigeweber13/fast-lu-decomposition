667ed6ef1924aeb36c18340834d1fe301e08e15d
Riley <rileyw13@protonmail.com>
2019-11-04 12:41:55 -0500

    added setw and set precision, outputs which matrix
    is being printed. Setw accommodates for the max character width of a float,
    even those that use scientific notation.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <iomanip>
 #include <iostream>
 
 #include "generate_test_matrices.hpp"
@@ -14,19 +15,23 @@ int main(){
 void print_matrix(Matrix m){
   for(size_t i = 0; i < m.size(); i++){
     for(size_t j = 0; j < m[0].size(); j++){
-      cout << m[i][j] << ", ";
+      cout << setw(10) << setprecision(4) << m[i][j] << ", ";
     }
     cout << endl;
   }
+  cout << endl;
 }
 
 void test(){
-  auto m = generate_matrix(10);
+  auto m = generate_matrix(8);
+  cout << "original matrix:" << endl;
   print_matrix(m);
 
   auto lu = lu_factorize(m);
   auto l = lu.first;
   auto u = lu.second;
+  cout << "lower diagonal:" << endl;
   print_matrix(l);
+  cout << "upper diagonal:" << endl;
   print_matrix(u);
 }
