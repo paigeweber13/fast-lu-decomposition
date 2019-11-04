a5f44a1f947695997f9b992a6a143992dc3a95df
Riley <rileyw13@protonmail.com>
2019-11-04 11:52:30 -0500

    moved print and test procedures to own functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 
 #include "generate_test_matrices.hpp"
+#include "lu_decomp.hpp"
 
 using namespace std;
 
+void test();
+
 int main(){
-  auto m = generate_matrix(10);
+  test();
+}
+
+void print_matrix(Matrix m){
   for(size_t i = 0; i < m.size(); i++){
     for(size_t j = 0; j < m[0].size(); j++){
       cout << m[i][j] << ", ";
     }
     cout << endl;
   }
+}
+
+void test(){
+  auto m = generate_matrix(10);
+  print_matrix(m);
+
+  auto lu = lu_factorize(m);
+  auto l = lu.first;
+  auto u = lu.second;
+  print_matrix(l);
+  print_matrix(u);
 }
