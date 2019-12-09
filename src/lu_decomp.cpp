e54841553bdfe6b61fcb69ee274724a052a42bc0
Riley <rileyw13@protonmail.com>
2019-12-08 23:53:32 -0500

    separated lu_factorize into sequential and parallel, improved comments

--- src/lu_decomp.cpp
+++ src/lu_decomp.cpp
@@ -1,26 +1,67 @@
 #include "lu_decomp.hpp"
 
-void lu_factorize(Matrix &m, omp_sched_t sched_type, size_t chunk_size){
+void lu_factorize(Matrix &m, omp_sched_t sched_type,
+                           size_t chunk_size){
+  // todo: start using this:
+  // const size_t threshold = 512;
+  // if (m.size() < threshold){
+  //   lu_factorize_sequential(m);
+  // }
+  // else {
+  //   lu_factorize_parallel(m, sched_type, chunk_size);
+  // }
+  lu_factorize_sequential(m);
+  // lu_factorize_parallel(m, sched_type, chunk_size);
+}
+
+void lu_factorize_sequential(Matrix &m, omp_sched_t sched_type,
+                           size_t chunk_size){
   double diag, target, multiplier;
 
   // each column depends on the column to the left
-  for (size_t j = 0; j < m[0].size(); j++){
-    diag = m[j][j];
+  for (size_t col = 0; col < m[0].size(); col++){
+    // for each column
+    diag = m[col][col];
+    for (size_t row = col+1; row < m.size(); row++){
+      // for each row under the diagonal
+
+      target = m[row][col];
+      multiplier = -target/diag;
+      for (size_t col_2 = col; col_2 < m[0].size(); col_2++){
+        // for each column (again) starting at the diagonal and moving right
+        m[row][col_2] = m[col][col_2] * multiplier + m[row][col_2];
+      }
+
+      m[row][col] = -multiplier;
+    }
+  }
+}
+
+void lu_factorize_parallel(Matrix &m, omp_sched_t sched_type,
+                           size_t chunk_size){
+  double diag, target, multiplier;
+
+  // each column depends on the column to the left
+  for (size_t col = 0; col < m[0].size(); col++){
+    // for each column
+    diag = m[col][col];
     #pragma omp parallel 
     {
     omp_set_schedule(sched_type, chunk_size);
     #pragma omp for collapse(1) schedule(runtime)
-    for (size_t i = j+1; i < m.size(); i++){
-      // these for loops will do everything UNDER the diagonal
-      // accessed in column-major order
+    for (size_t row = col+1; row < m.size(); row++){
+      // for each row under the diagonal
+      // these can be done independently WITHIN THIS FOR LOOP
 
-      target = m[i][j];
+      target = m[row][col];
       multiplier = -target/diag;
-      for (size_t k = j; k < m[0].size(); k++){
-        m[i][k] = m[j][k] * multiplier + m[i][k];
+      for (size_t col_2 = col; col_2 < m[0].size(); col_2++){
+        // for each column (again) starting at the diagonal and moving right
+        // these can be done independently
+        m[row][col_2] = m[col][col_2] * multiplier + m[row][col_2];
       }
 
-      m[i][j] = -multiplier;
+      m[row][col] = -multiplier;
     }
     }
   }
--- src/lu_decomp.hpp
+++ src/lu_decomp.hpp
@@ -10,4 +10,7 @@ using namespace std;
 namespace ub = boost::numeric::ublas;
 
 void lu_factorize(Matrix &m, omp_sched_t sched_type, size_t chunk_size);
+void lu_factorize_sequential(Matrix &m);
+void lu_factorize_parallel(Matrix &m, omp_sched_t sched_type,
+                           size_t chunk_size);
 bool check_lu_correctness(ub::matrix<double> input, Matrix lu);
